3cf2681fe53c8357c4269692f3e523c4fc307869
Victor <victorportolani@gmail.com>
2025-09-16 14:43:28 -0300

    atualiza o readme do projeto e tem o componente da navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import './index.css'
+import 'bootstrap'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  
   <StrictMode>
-    <App />
+    <BrowserRouter> {/* AQUI! */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
